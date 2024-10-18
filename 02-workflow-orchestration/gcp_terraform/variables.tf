3700cdf75034b2aba4b959788abda7f5ab3e3711
Philipp Hardy <philipp.hardy@unibw.de>
2024-10-18 21:38:10 +0200

    init

---
+++ 02-workflow-orchestration/gcp_terraform/variables.tf
@@ -0,0 +1,78 @@
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+  default     = "mage-data-prep"
+}
+
+variable "container_cpu" {
+  description = "Container cpu"
+  default     = "2000m"
+}
+
+variable "container_memory" {
+  description = "Container memory"
+  default     = "2G"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The name of the project"
+  default     = "sound-psyche-437519-k6"
+}
+
+variable "region" {
+  type        = string
+  description = "The default compute region"
+  default     = "us-west3"
+}
+
+variable "zone" {
+  type        = string
+  description = "The default compute zone"
+  default     = "us-west3-a"
+}
+
+variable "repository" {
+  type        = string
+  description = "The name of the Artifact Registry repository to be created"
+  default     = "mage-data-prep"
+}
+
+variable "database_user" {
+  type        = string
+  description = "The username of the Postgres database."
+  default     = "mageuser"
+}
+
+variable "database_password" {
+  type        = string
+  description = "The password of the Postgres database."
+  sensitive   = true
+  default     = "postgres"
+}
+
+variable "docker_image" {
+  type        = string
+  description = "The docker image to deploy to Cloud Run."
+  default     = "mageai/mageai:latest"
+}
+
+variable "domain" {
+  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
+  type        = string
+  default     = ""
+}
+
+variable "ssl" {
+  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
+  type        = bool
+  default     = false
+}
+
+variable "credentials" {
+  description = "My Credentials"
+  default     = "C:/Users/phili/Nextcloud/Projekte/HardyTec/Wissen/Data Engineering/Data Engineering Zoomcamp/sound-psyche-437519-k6-3889936f8946.json"
+
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
